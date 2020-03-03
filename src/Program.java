846118eed625da66f0738ed8d156f9b14623f885
kolaiStudent <55719782+kolaiStudent@users.noreply.github.com>
2020-03-03 23:07:27 +0200

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -1,38 +1,17 @@
-import java.util.Scanner;
-import java.io.*;
 public class Program {
 
-    public static void main (String [] args) throws TimeoutException, InterruptedException {
-        Action test = new ActionClass(); // создаём пустой класс, который будет реализовывать наш интерфейс.
-        boolean working = true;
-        //вечный цикл, программы, который делает действия в зависимости от числа, которое вы ввели.
-        while (working){
-            Scanner scan = new Scanner(System.in);
-            System.out.println("введите 2, чтоб начать деление с учётом времени\nвведите 1 если просто хотите начать деление\nвведите 0, чтобы выйти из программы");
-            String k = scan.next();
-            // вызываем метод, который выбрасывает ошибку, если мы слишком долго вводим числа
-            if (k.equals("2")){
-                ActionWithTimeout t = new ActionWithTimeout();
-                t.actionWithTimeout(test,100);
-            }
-            // просто вызываем doAction нашего интерфейса
-            else if (k.equals("1")){
-                System.out.println(test.doAction(5000000));
-            }
-            // выходим из программы
-            else if (k.equals("0")){
-                working=false;
-            }
-            // если пользователь вводит что-то не то
-            else {
-                System.out.println("вы что-то не то ввели, попробуйте снова");
-            }
-            //отделение каждого цикла программы, чтоб было проще смотреть
-            System.out.println(" ");
-            System.out.println("****************************************************************************");
-            System.out.println("****************************************************************************");
-            System.out.println("****************************************************************************");
-            System.out.println(" ");
-        }
+    public static void main (String [] args) throws TimeoutException, InterruptedException, Exception {
+        // просто метод ду
+        Action<Integer, String, Exception> test = new ActionClass();
+        System.out.println(test.does(4));
+        // метод ду с проверкой по времени когда времени достаточно
+        ActionWithTimeout <Integer> niceTimeout = new ActionWithTimeout<>();
+        niceTimeout.actionWithTimeout(test, 5 , 5000);
+        // метод ду с проверкой по времени когда времени не достаточно (процесс не прерывается, но в конце выдаёт ошибку)
+        ActionWithTimeout <Integer> badTimeout = new ActionWithTimeout<>();
+        badTimeout.actionWithTimeout(test, 5 , 1);
+        // метод ду который сам выбрасывает ошибку
+        Action<String, String, Exception> fail = new ActionClass();
+        System.out.println(fail.does(" s"));
     }
 }

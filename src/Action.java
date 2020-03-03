52d5aa1cb9a5d18ca8f2faf5dc99a9e64e6ecdcb
kolaiStudent <55719782+kolaiStudent@users.noreply.github.com>
2020-03-03 23:07:57 +0200

    Update Action.java

--- src/Action.java
+++ src/Action.java
@@ -1,15 +1,15 @@
-import java.util.ArrayList;
+public interface Action<P, R extends String, E extends Exception> {
+    default R does (P p) throws E
+    {
+        R r = (R) "dunno";
+        E e = (E) new Exception("exept");
+        Integer o=0;
+        for (int i = 0; i<5000; i++){
+            double k = Math.random();
+    }
+        if (p.getClass().isInstance(o)) return r;
+        else throw e;
 
 
-public interface Action {
-    default <T extends Number> long doAction (T n) throws ArithmeticException{
-        ArrayList<Integer> mylist = new ArrayList<>();
-        long time = System.currentTimeMillis();
-        for (int i = 0; i<(int)n; i++){
-            mylist.add((int) (Math.random()*200-100));
-        }
-        time-=System.currentTimeMillis();
-        return time*-1;
     }
 }
-
